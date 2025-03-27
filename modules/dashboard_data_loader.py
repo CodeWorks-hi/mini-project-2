32b46b7d5653aeb0469ebaae54b8f7319ae1e486
codekookiz <codekookiz@gmail.com>
2025-03-27 16:34:32 +0900

    add

--- modules/dashboard_data_loader.py
+++ modules/dashboard_data_loader.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import streamlit as st
-import os
 
 @st.cache_data
 def load_csv(path):
-    base_dir = os.path.dirname(__file__)  # 현재 파일의 위치
     try:
-        return pd.read_csv(os.path.join(base_dir, path))
+        return pd.read_csv(path)
     except Exception as e:
         st.error(f"CSV 파일 로드 중 오류 발생: {str(e)}")
         return None
 
 def load_and_merge_export_data():
-    df_h = load_csv("../data/processed/현대_지역별수출실적_전처리.CSV")
-    df_k = load_csv("../data/processed/기아_지역별수출실적_전처리.CSV")
+    df_h = load_csv("data/processed/현대_지역별수출실적_전처리.CSV")
+    df_k = load_csv("data/processed/기아_지역별수출실적_전처리.CSV")
     if df_h is None or df_k is None:
         return None
 
@@ -24,16 +22,16 @@ def load_and_merge_export_data():
     return pd.concat([df_h, df_k], ignore_index=True)
 
 def load_hyundai_factory_data():
-    df = load_csv("../data/processed/현대_해외공장판매실적_전처리.CSV")
+    df = load_csv("data/processed/현대_해외공장판매실적_전처리.CSV")
     if df is not None:
         df["브랜드"] = "현대"
     return df
 
 def load_kia_factory_data():
-    df = load_csv("../data/processed/기아_해외공장판매실적_전처리.CSV")
+    df = load_csv("data/processed/기아_해외공장판매실적_전처리.CSV")
     if df is not None:
         df["브랜드"] = "기아"
     return df
 
 def load_location_data():
-    return load_csv("../data/세일즈파일/지역별_위치정보.csv")
+    return load_csv("data/세일즈파일/지역별_위치정보.csv")
