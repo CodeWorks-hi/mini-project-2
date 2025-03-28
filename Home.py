d3426796ceaee2dfa66e53c72395c8d141263e1f
nhchoi2 <choi1278@gmail.com>
2025-03-28 16:06:16 +0900

    1

--- Home.py
+++ Home.py
@@ -27,7 +27,7 @@ logging.basicConfig(filename='error_log.txt', level=logging.ERROR)
 # ✅ 탭 UI 구성
 tabs = st.tabs([
     " 대시보드", " 생산 관리", " 재고 관리",
-    " 판매 관리",   "예측 시스템"," 분석 리포트", "AI 예측 및 분석"," 인트로","원본데이터 확인"
+    " 판매 관리",   "예측 시스템","AI 예측 및 분석"," 분석 리포트", " 인트로","원본데이터 확인"
 ])
 
 tab_modules = [
@@ -36,8 +36,9 @@ tab_modules = [
     ("modules.inventory", "inventory_ui"), #재고
     ("modules.export", "export_ui"),  # 수출관리
     ("modules.prediction", "prediction_ui"), # 예측
-    ("modules.analytics", "analytics_ui"), # 분석관리
+
     ("modules.recommendations", "recommendations_ui"), #추천 
+    ("modules.analytics", "analytics_ui"), # 분석관리
     ("modules.intro", "intro_ui"), # 인트로
     ("modules.data", "data_preview_ui"), # 인트로
 
